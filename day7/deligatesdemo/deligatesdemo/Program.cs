72cbc8667ba9b2db633d30634ac39f705824a1ae
Arpit Pandey <arpit.pandey@taazaa.com>
2021-03-24 13:14:01 +0530

    day 7 extension method and delegates

---
+++ day7/deligatesdemo/deligatesdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace deligatesdemo
+{
+    class Program
+    {
+        delegate void del(string str);
+        
+        static void Main(string[] args)
+        {
+            del d = new del(notify);
+            abc(d);
+            d = greet;
+            d("you are great");
+        }
+        static void notify(string arp)
+        {
+            Console.WriteLine($"notification from : {arp}");
+        }
+        static void greet(string arp)
+        {
+            
+                Console.WriteLine($"information from : {arp}");
+            
+        }
+        static void abc(del d)
+        {
+            d("HEY");
+        }
+    }
+}
