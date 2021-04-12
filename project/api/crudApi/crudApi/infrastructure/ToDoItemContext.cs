7f2e18bb5540058c33e9836cce8151745f35c87c
Arpit Pandey <arpit.pandey@taazaa.com>
2021-04-12 17:30:32 +0530

    Api using entity framework

---
+++ project/api/crudApi/crudApi/infrastructure/ToDoItemContext.cs
@@ -0,0 +1,21 @@
+﻿using crudApi.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudApi.infrastructure
+{
+    public class ToDoItemContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        //fluent API
+        //we will have 1000ands or controller so we were hard coding in overriding Onconfiguring but in this senario it will pck up the connection string according to the project mean where it get open.
+        public ToDoItemContext(DbContextOptions<ToDoItemContext> options):base(options)
+        {
+            
+        }
+    }
+}
