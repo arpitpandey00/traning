0b6039537f7eb533e028ee7b6bed5bcc4f91f000
Arpit Pandey <arpit.pandey@taazaa.com>
2021-04-12 17:26:38 +0530

    demo api

---
+++ MyFirstApi/MyFirstApi/Controllers/operationcontroller.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class operationcontroller : ControllerBase
+    {
+        [HttpGet]
+        public string prod()
+        {
+            return "hello";
+        }
+        [HttpGet("abc")]
+        public string prod(string name)
+        {
+            return "hello" + name;
+        }
+
+        [HttpPost("updateprod")]
+        public Product AddProduct(Product p)
+        {
+            p.Price = p.Price * 1000;
+            return p;
+        }
+
+        [HttpGet("getproducts")]
+        public List<Product> GetProducts()
+        {
+           // var b = List<Product>();
+            ProjectReprository p = new ProjectReprository();
+            return p.getProduct(); 
+
+        }
+        [HttpGet("IDfind")]
+        public Product findId(int a)
+        {
+            ProjectReprository p = new ProjectReprository();
+            return p.findProduct(a);
+        }
+        [HttpPost("addproduct")]
+        public List<Product> Addproduct(Product p)
+        {
+            ProjectReprository proj = new ProjectReprository();
+            proj.addingProducts(p);
+            return proj.getProduct();
+
+        }
+    }
+
+}
---
+++ MyFirstApi/MyFirstApi/ProjectReprository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi
+{
+    public class ProjectReprository
+    {
+
+        public static List<Product> products = new List<Product>
+        {
+                new Product
+                {
+                    Id = 1,
+                    Title = "abc",
+                    Price = 100
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "xyz",
+                    Price = 200
+                },
+
+         };
+         public List<Product> getProduct()
+         {
+            return products;
+         }
+        public void addingProducts(Product p)
+        {
+            products.Add(p);
+        }
+        public Product findProduct(int id)
+        {
+            var a = new Product();
+            foreach(Product p in products) { 
+            if(p.Id == id)
+            {
+                    a = p;
+            }
+            }
+            return a;
+        }
+    }
+}
