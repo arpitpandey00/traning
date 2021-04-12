a002f1410cc5eb9efb4d0350284befa001592815
Arpit Pandey <arpit.pandey@taazaa.com>
2021-04-12 17:23:51 +0530

    database first approach

---
+++ databasefirst/databasefirst/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databasefirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            databasefirstEntities dbnew = new databasefirstEntities();
+            Product producttb = new Product()
+            {
+                id=4,
+                title="scale",
+                price=12
+            };
+            Detail detailtb = new Detail()
+            {
+                id=4,
+                vendor="doms",
+                quantity=40
+            };
+            dbnew.Details.Add(detailtb);
+            dbnew.Products.Add(producttb);
+            dbnew.SaveChanges();
+            var x = dbnew.Details.Include("Product");
+            x.ToList().ForEach(
+            i => {Console.WriteLine("name : "+i.vendor);
+                }) ;
+            Console.ReadKey(true);
+        }
+    }
+}
